170e46d3e74c5a93d4de2d5e23b4dfc0f14c0a0a
FACEPrep-ProGrad <61002120+FACEPrep-ProGrad@users.noreply.github.com>
2020-03-16 15:19:15 +0530

    Create ddl-pkl.sql

---
+++ ddl-pkl.sql
@@ -0,0 +1,33 @@
+-- PROGRESSION - 1
+
+
+-- 1. **Create table city**
+
+-- 2. **Create table referee**
+
+-- 3. **Create table innings**
+
+-- 4. **Create table extra_type**
+
+-- 5. **Create table skill**
+
+-- 6. **Create table team**
+
+-- 7. **Create table player**
+
+-- 8. **Create table venue**
+
+-- 9. **Create table event**
+-- 10. **Create table extra_event**
+
+-- 11. **Create table outcome**
+
+-- 12. **Create table game**
+
+-- 13. **Drop table city**
+
+-- 14. **Drop table innings**
+
+-- 15. **Drop table skill**
+
+-- 16. **Drop table extra_type**
